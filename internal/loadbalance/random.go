8e4c447645810989eb61691c220c362877c1a1d2
huanheart <luokaitai306@gmail.com>
2026-02-11 23:52:38 +0800

    随机数

--- internal/loadbalance/random.go
+++ internal/loadbalance/random.go
@@ -3,15 +3,27 @@ package loadbalance
 import (
 	"kamaRPC/internal/registry"
 	"math/rand"
+	"sync"
 	"time"
 )
 
-type Random struct{}
+type Random struct {
+	r *rand.Rand
+	m sync.Mutex
+}
+
+func NewRandom() *Random {
+	return &Random{
+		r: rand.New(rand.NewSource(time.Now().UnixNano())),
+	}
+}
 
 func (r *Random) Select(list []registry.Instance) registry.Instance {
 	if len(list) == 0 {
 		return registry.Instance{}
 	}
-	rand.Seed(time.Now().UnixNano())
-	return list[rand.Intn(len(list))]
+
+	r.m.Lock()
+	defer r.m.Unlock()
+	return list[r.r.Intn(len(list))]
 }
